886df9ee50a12d0eca5ccb088412dd794f5de209
Theneau Maxime <theneau.maxime@gmail.com>
2023-10-10 15:13:09 +0200

    Feat: install pm2

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout from '../components/layout';
 import '../styles/globals.scss';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   // const router = useRouter();
@@ -18,6 +19,21 @@ function MyApp({ Component, pageProps }) {
         <meta name="google-site-verification" content="yObJd5noBtjUBky_GRbOOETV42Q9qAHf7w00PPz1-ss" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
+      <Script 
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script 
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+          `
+        }}
+      />
       <Layout {...pageProps}>
         {/* {animateTransition && <div className="transition" aria-hidden="true" />} */}
         <Component {...pageProps} />
