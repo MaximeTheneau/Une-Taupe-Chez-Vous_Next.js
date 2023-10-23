14b248285e73446f52fb4b3946e916b3b5a9f086
Theneau Maxime <theneau.maxime@gmail.com>
2023-10-23 18:49:49 +0200

    Feat: test

--- src/middleware/FormMiddleware.jsx
+++ src/middleware/FormMiddleware.jsx
@@ -1,15 +1,19 @@
-export default function middleware(req, apiPath, handleResponse200, handleResponseError) {
+export default function formMiddleware(req, apiPath, handleResponse200, handleResponseError) {
   const requestOptions = {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'https://back.unetaupechezvous.fr' },
-    credentials: 'include',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(req),
   };
   fetch(`${process.env.NEXT_PUBLIC_API_URL}${apiPath}`, requestOptions)
-    .then((response) => {
-      if (response.ok) {
-        return handleResponse200();
-      }
-      throw new Error('Une erreur est survenue');
+  .then((response) => {
+    if (response.ok) {
+      handleResponse200();
+    } else if (response.status === 500) {
+      handleResponseError('Serveur en panne, veuillez revenir plus tard');
+    } else {
+      response.json().then((data) => {
+        handleResponseError(data.erreur);
+      });
+    }
     });
 }
