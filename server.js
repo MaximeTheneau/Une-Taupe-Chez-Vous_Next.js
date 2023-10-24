4ee889a31e27b0e8dff61df344e038fda2745eda
Theneau Maxime <theneau.maxime@gmail.com>
2023-10-24 15:16:14 +0200

    test correction

--- server.js
+++ server.js
@@ -41,3 +41,12 @@ app.get('/api/build-export-endpoint', async (req, res) => {
     res.status(500).json({ message: 'Failed to trigger build and export.' });
   }
 });
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://unetaupechezvous.fr');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Max-Age', '3600');
+  next();
+});
