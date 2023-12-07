919315e01cfad1bd845178daa74cd819746c9f9e
Theneau Maxime <theneau.maxime@gmail.com>
2023-12-07 09:41:26 +0100

    Feat: Change route Sitemap Index

--- scripts/sitemap/sitemapIndex.script.js
+++ scripts/sitemap/sitemapIndex.script.js
@@ -5,12 +5,11 @@ const generateSitemapIndex = () => {
   const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
     <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <sitemap>
-        <loc>${urlFront}/sitemap_images.xml</loc>
+        <loc>${urlFront}/sitemap/sitemap_images.xml</loc>
       </sitemap>
       <sitemap>
-        <loc>${urlFront}/sitemap_all_links.xml</loc>
+        <loc>${urlFront}/sitemap/sitemap_all_links.xml</loc>
       </sitemap>
-      <!-- Ajoutez d'autres sitemaps ici -->
     </sitemapindex>`;
 
   fs.writeFileSync('./public/sitemap/sitemap_index.xml', sitemapIndex);
