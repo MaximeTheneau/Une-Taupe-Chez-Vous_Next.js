8cc2d1ef2d437216988f8c7491496a0a2b536c1b
Theneau Maxime <theneau.maxime@gmail.com>
2024-01-09 13:54:37 +0100

    Feat: change build

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'standalone',
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
