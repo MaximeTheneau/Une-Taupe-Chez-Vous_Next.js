3bbd4781618084bbc55bb7a1c564293e2dc0862d
Theneau Maxime <theneau.maxime@gmail.com>
2024-01-10 13:46:43 +0100

    Feat: change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require('url')
 const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
-const port = 3002
+const port = 3001
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
