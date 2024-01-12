17fa3fa2a0d591157527c59e58534c1bc5219c2b
Theneau Maxime <theneau.maxime@gmail.com>
2024-01-12 15:48:48 +0100

    Feat: error build

--- src/pages/api/build-export-endpoint.js
+++ src/pages/api/build-export-endpoint.js
@@ -1,13 +1,13 @@
 export default async function handler(req, res) {
-  try {
-    console.log(req);
-    // eslint-disable-next-line global-require
-    const { exec } = require('child_process');
-    exec('npm run build');
+  console.log(req);
+  console.error(res);
+  // try {
+  //   // eslint-disable-next-line global-require
+  //   const { exec } = require('child_process');
+  //   exec('npm run build');
 
-    res.status(200).json({ message: req.headers });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Failed to trigger build and export.' });
-  }
+  //   res.status(200).json({ message: req.headers });
+  // } catch (error) {
+  //   res.status(500).json({ message: 'Failed to trigger build and export.' });
+  // }
 }
