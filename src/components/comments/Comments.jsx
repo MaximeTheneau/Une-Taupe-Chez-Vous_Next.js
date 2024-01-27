5f3ec95d81893d18b01dcc641fe5a16a3ca10400
Theneau Maxime <theneau.maxime@gmail.com>
2024-01-27 16:21:23 +0100

    Feat: Created reply for comment

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -20,7 +20,6 @@ export default function Comments({ posts }) {
       posts: posts.id,
     },
   });
-
   const handleBlur = (e) => {
     if (!e.target.value) return;
     fetch(`${process.env.NEXT_PUBLIC_API_URL}comments/verify_email`, {
@@ -147,6 +146,32 @@ export default function Comments({ posts }) {
                 <time dateTime={comment.createdAt}>{formattedDate(comment.createdAt)}</time>
               </p>
               <p className={`${styles['comments__list__item--text']}`}>{comment.comment}</p>
+              {comment.replies?.length > 0 && (
+              <>
+                <ul className={styles.comments__list__responses}>
+                  <li className={styles.comments__list__responses__item}>
+                    <h4>Réponses :</h4>
+                  </li>
+                  {comment.replies?.map((response) => (
+                    <li className={styles.comments__list__responses__item} key={response.id}>
+                      <p className={`${styles['comments__list__item--user']}`}>
+                        {comment.User}
+                        {' '}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none">
+                          <path d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                      </p>
+                      <p className={`${styles['comments__list__item--user']}`}>
+                        le
+                        {' '}
+                        <time dateTime={comment.createdAt}>{formattedDate(comment.createdAt)}</time>
+                      </p>
+                      <p className={`${styles['comments__list__item--text']}`}>{comment.comment}</p>
+                    </li>
+                  ))}
+                </ul>
+              </>
+              )}
             </li>
           ))}
         </ul>
