f30a4d32b10b1868bc55a8aebac22752f4494e7e
Theneau Maxime <theneau.maxime@gmail.com>
2024-02-12 14:38:29 +0100

    Feat: gg analytic

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,6 +7,20 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="google-site-verification" content={process.env.NEXT_PUBLIC_GOOGLE_WEBMASTER_ID} />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID});
+              `,
+          }}
+        />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
       {/* <CookiesModal /> */}
