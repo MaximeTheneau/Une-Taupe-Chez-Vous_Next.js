f0bd500cc9991a2eeafbf6c0a0f903634ddfb31c
Theneau Maxime <theneau.maxime@gmail.com>
2024-02-19 13:44:17 +0100

    test: webhook

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -15,6 +15,7 @@ export default async function handler(req, res) {
   if (`sha256=${signature}` !== calculatedSignature) {
     console.error('Invalid signature.');
     res.status(401).send(`Invalid signature${signature} ${calculatedSignature}`);
+
     return;
   }
 
