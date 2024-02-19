b949a6e3bdd5761188fdd43e6d32c775ab88c5c3
Theneau Maxime <theneau.maxime@gmail.com>
2024-02-19 13:25:14 +0100

    test: webhook

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -12,11 +12,11 @@ export default async function handler(req, res) {
   hmac.update(body);
   const calculatedSignature = `sha256=${hmac.digest('hex')}`;
 
-  // if (signature !== calculatedSignature) {
-  //   res.status(401).send(signature, calculatedSignature);
-
-  //   return;
-  // }
+  if (signature !== calculatedSignature) {
+    console.error('Invalid signature.');
+    res.status(401).send(`Invalid signature${signature} ${calculatedSignature}`);
+    return;
+  }
 
   const branch = 'main';
   const gitStash = spawn('git', ['stash']);
