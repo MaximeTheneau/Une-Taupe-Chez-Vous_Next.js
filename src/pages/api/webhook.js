d626b4f79e337b02c619ea5e895a2177fc0135a9
Theneau Maxime <theneau.maxime@gmail.com>
2024-02-19 12:19:53 +0100

    test webhook

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -44,6 +44,7 @@ export default async function handler(req, res) {
       exec('npm run build', (error, stdout) => {
         if (error) {
           console.error(`Error running npm run build: ${error}`);
+
           return;
         }
         console.log(`npm run build output: ${stdout}`);
