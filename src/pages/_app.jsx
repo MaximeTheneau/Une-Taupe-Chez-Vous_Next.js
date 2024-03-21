2eab9bca58c889fa703534c3ead18619c5502a18
Theneau Maxime <theneau.maxime@gmail.com>
2024-03-21 12:29:18 +0100

    Feat:change favicon.ico

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="google-site-verification" content={process.env.NEXT_PUBLIC_GOOGLE_WEBMASTER_ID} />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/favicon/apple-icon.png" key="apple" />
         <link rel="icon" href="/favicon/favicon-16x16.png" sizes="16x16" key="icon16" />
         <link rel="icon" href="/favicon/favicon-32x32.png" sizes="32x32" key="icon32" />
