42d533836a7b0c9293e3b4c744655d2c837ce5e5
Theneau Maxime <theneau.maxime@gmail.com>
2024-03-21 12:03:33 +0100

    Feat: add favicon and manifect.js

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -9,6 +9,10 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="google-site-verification" content={process.env.NEXT_PUBLIC_GOOGLE_WEBMASTER_ID} />
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/favicon/apple-icon.png" key="apple" />
+        <link rel="icon" href="/favicon/favicon-16x16.png" sizes="16x16" key="icon16" />
+        <link rel="icon" href="/favicon/favicon-32x32.png" sizes="32x32" key="icon32" />
+        <link rel="icon" href="/favicon/favicon-96x96.png" sizes="96x96" key="icon96" />
       </Head>
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
       {/* <CookiesModal /> */}
