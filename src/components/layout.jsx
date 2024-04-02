6827c1f567711f5993f4dca037e71859855584bd
Theneau Maxime <theneau.maxime@gmail.com>
2024-04-02 16:10:14 +0200

    Feat: optimize font/local

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,14 +1,31 @@
+import localFont from 'next/font/local';
 import Navbar from './navbar/Navbar';
 import Footer from './footer/footer';
 
+const title = localFont({
+  src: [
+    { path: '../asset/font/title/title.woff2' },
+    { path: '../asset/font/title/title.ttf' },
+  ],
+  display: 'swap',
+  variable: '--font-title',
+});
+const principal = localFont({
+  src: [
+    { path: '../asset/font/principal/principal.woff2' },
+    { path: '../asset/font/principal/principal.ttf' },
+  ],
+  display: 'swap',
+  variable: '--font-principal',
+
+});
 export default function Layout({ children }) {
   return (
     <>
-
       <header>
         <Navbar />
       </header>
-      <main>
+      <main className={` ${principal.variable} ${title.variable}`}>
         {children}
       </main>
       <footer>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,6 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-
     </Html>
   );
 }
