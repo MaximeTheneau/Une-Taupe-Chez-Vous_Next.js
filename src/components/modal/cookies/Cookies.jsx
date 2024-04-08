e4481d3986b5ea8f67e194e1d69fcc1db63d1b2e
Theneau Maxime <theneau.maxime@gmail.com>
2024-04-08 11:27:10 +0200

    Feat: change css modal cookies

--- src/components/modal/cookies/Cookies.jsx
+++ src/components/modal/cookies/Cookies.jsx
@@ -3,67 +3,59 @@ import style from '../Modal.module.scss';
 import CookieChoice from './CookieChoice';
 
 export default function CookiesModal() {
-  const [cookiesModal, setCookiesModal] = useState(false);
+  const [cookiesModal, setCookiesModal] = useState(null);
   const [state, setState] = useState({
     cookiesGoogle: false,
-    cookiesWebmaster: '',
+    cookiesWebmaster: false,
     cookiesChoice: false,
-    cookiesAll: '',
+    cookiesAll: false,
   });
 
   useEffect(() => {
-    const scriptInit = document.createElement('script');
-    scriptInit.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`;
-    scriptInit.async = true;
+    setTimeout(() => {
+      if (window.localStorage.getItem('cookiesModal') === null && cookiesModal === null) {
+        setCookiesModal(false);
+      }
 
-    const script = document.createElement('script');
-    script.id = 'google-analytics';
+      const scriptInit = document.createElement('script');
+      scriptInit.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`;
+      scriptInit.async = true;
 
-    const cookiesGoogleValue = window.localStorage.getItem('cookiesGoogle');
-    const consentSettings = cookiesGoogleValue ? {
-      ad_storage: 'granted',
-      ad_user_data: 'granted',
-      ad_personalization: 'granted',
-      analytics_storage: 'granted',
-    } : {
-      ad_storage: 'denied',
-      ad_user_data: 'denied',
-      ad_personalization: 'denied',
-      analytics_storage: 'denied',
-    };
-    const scriptCode = `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('consent', 'update', ${JSON.stringify(consentSettings)});
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
-          page_path: window.location.pathname
-        });
-        gtag('js', new Date());
-    `;
-    script.textContent = scriptCode;
+      const script = document.createElement('script');
+      script.id = 'google-analytics';
 
-    document.body.appendChild(script);
-    document.body.appendChild(scriptInit);
+      const cookiesGoogleValue = window.localStorage.getItem('cookiesGoogle');
+      const consentSettings = cookiesGoogleValue ? {
+        ad_storage: 'granted',
+        ad_user_data: 'granted',
+        ad_personalization: 'granted',
+        analytics_storage: 'granted',
+      } : {
+        ad_storage: 'denied',
+        ad_user_data: 'denied',
+        ad_personalization: 'denied',
+        analytics_storage: 'denied',
+      };
+      const scriptCode = `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('consent', 'update', ${JSON.stringify(consentSettings)});
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+            page_path: window.location.pathname
+          });
+          gtag('js', new Date());
+      `;
+      script.textContent = scriptCode;
 
-    return () => {
-      document.body.removeChild(script);
-      document.body.removeChild(scriptInit);
-    };
-  }, [state.cookiesGoogle]);
+      document.head.appendChild(scriptInit);
+      document.head.appendChild(script);
 
-  useEffect(() => {
-    setState({
-      ...state,
-      cookiesGoogle: window.localStorage.getItem('cookiesGoogle'),
-    });
-    const cookiesModalParam = window.localStorage.getItem('cookiesModal');
-    if (cookiesModalParam === null) {
-      setTimeout(() => {
-        setCookiesModal(true);
-        window.localStorage.setItem('cookiesModal', true);
-      }, 1000);
-    }
-  }, []);
+      return () => {
+        document.head.removeChild(scriptInit);
+        document.head.removeChild(script);
+      };
+    }, 1000);
+  }, [state.cookiesGoogle, cookiesModal]);
 
   const toggleCookies = (field, value) => {
     setState((prevState) => ({
@@ -72,19 +64,13 @@ export default function CookiesModal() {
     }));
     window.localStorage.setItem(field, value);
   };
+
   const handleAcceptCookies = () => {
     window.localStorage.setItem('cookiesModal', true);
-    window.localStorage.setItem('cookiesModal', cookiesModal);
+    window.localStorage.setItem('cookiesGoogle', true);
     setState({ ...state, cookiesGoogle: true });
-
-    setCookiesModal(false);
+    setCookiesModal(true);
   };
-
-  useEffect(() => {
-    if (state.cookiesGoogle === 'true') {
-      handleAcceptCookies();
-    }
-  }, [state.cookiesGoogle]);
 
   const handleRefuseCookies = () => {
     setCookiesModal(false);
@@ -93,8 +79,7 @@ export default function CookiesModal() {
   };
 
   return (
-
-    cookiesModal && (
+    cookiesModal === false && (
     <div className={style.cookies}>
       {state.cookiesChoice ? (
         <div className={`card ${style.cookies__choice}`}>
@@ -113,17 +98,6 @@ export default function CookiesModal() {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>Tout cocher</td>
-                <td>
-                  <i
-                    className={`icon-${state.cookiesAll ? 'confirmation' : 'error'}`}
-                      // onClick={toggleCookies('all')}
-                    aria-labelledby="button-label"
-                  />
-                </td>
-              </tr>
-
               <CookieChoice
                 label="Google Analytics GU4"
                 checked={state.cookiesGoogle}
@@ -144,20 +118,11 @@ export default function CookiesModal() {
       ) : (
         <>
           <h2>
-            Vos choix en matière de cookies sur ce site
+            À vous de choisir !
           </h2>
           <p>
-            Un cookie est un petit fichier informatique, un traceur, déposé et lu
-            par exemple lors de la consultation d&apos;un site internet. Les cookies
-            sont importants pour le bon fonctionnement d&apos;un site. Ainsi afin
-            d&apos;améliorer votre expérience, nous utilisons des cookies pour
-            collecter les statistiques en vue d&apos;optimiser les fonctionnalités
-            du site et adapter le contenu à vos centres d&apos;intérêt. Cliquez sur
-            « Accepter » pour accepter les cookies et poursuivre directement sur
-            le site, cliquez sur « Refuser » pour refuser ou cliquez sur
-            « Personnaliser » pour consulter en détail les descriptions des
-            types de cookies et choisir ceux que vous voulez accepter lorsque
-            vous visitez le site.
+            En poursuivant votre navigation, vous acceptez l&apos;utilisation des cookies
+            pour nous aider à améliorer notre site internet. À vous de choisir !
           </p>
           <div className={style.cookies__button}>
             <button
