b9a92e65401d358e7acbe62308de2e22ad59daf0
Theneau Maxime <theneau.maxime@gmail.com>
2024-04-08 15:00:28 +0200

    Feat: change modal cookies

--- src/components/modal/cookies/Cookies.jsx
+++ src/components/modal/cookies/Cookies.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import style from '../Modal.module.scss';
 import CookieChoice from './CookieChoice';
 
