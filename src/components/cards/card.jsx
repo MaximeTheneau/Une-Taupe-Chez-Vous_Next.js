14d45b26a731dfb8add89c22f92a9486485249e6
Theneau Maxime <theneau.maxime@gmail.com>
2024-04-11 17:40:52 +0200

    Feat: test image parformance cards

--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -11,9 +11,8 @@ export default function Card({ card }) {
         rel="preload"
       >
         <img
-          src={`${process.env.NEXT_PUBLIC_CLOUD_URL}/c_thumb,w_330,q_70/${process.env.NEXT_PUBLIC_CLOUD_FILE_KEY}/${card.imgPost}.webp`}
+          src={`${process.env.NEXT_PUBLIC_CLOUD_URL}/c_thumb,g_face,w_330,h_330,q_auto:eco/${process.env.NEXT_PUBLIC_CLOUD_FILE_KEY}/${card.imgPost}.webp`}
           alt={card.altImg || card.title}
-          quality={70}
           width={330}
           height={330}
           loading="lazy"
