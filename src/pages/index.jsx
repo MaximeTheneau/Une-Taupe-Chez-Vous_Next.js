435d0243ce398be8390f0a8f626ade356c4e8f58
Theneau Maxime <theneau.maxime@gmail.com>
2024-04-12 17:45:49 +0200

    Feat: test iamge perf

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -67,12 +67,14 @@ export default function Home({
               height={accueil.imgHeight}
               priority
             />
+            <div className={styles.home__imagesFull__image__text}>
+              <h1>
+                {accueil.title}
+              </h1>
+              <p className={styles['home__imagesFull__text--paragraph']} dangerouslySetInnerHTML={{ __html: accueil.contentsHTML }} />
+              <DevisButton />
+            </div>
           </div>
-          <h1>
-            {accueil.title}
-          </h1>
-          <div className={styles['home__imagesFull__text--paragraph']} dangerouslySetInnerHTML={{ __html: accueil.contentsHTML }} />
-          <DevisButton />
         </div>
       </div>
       <section className={styles.home}>
