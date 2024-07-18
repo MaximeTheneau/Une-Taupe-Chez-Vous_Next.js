02d324a86d1b82da69b8fd59c90b39e0453cd6a5
Theneau Maxime <theneau.maxime@gmail.com>
2024-07-18 19:44:53 +0200

    Feat: create adsense components and cookies

--- src/components/adsense/ArticlesAdsense.jsx
+++ src/components/adsense/ArticlesAdsense.jsx
@@ -1,22 +1,21 @@
 export default function ArticlesAdsense() {
-  console.log(window.localStorage.getItem('cookiesAdsense'));
-
-  // && (
-  // <div style={{ display: 'block', textAlign: 'center' }}>
-  //   <ins
-  //     className="adsbygoogle"
-  //     style={{ display: 'block', textAlignt: 'center' }}
-  //     data-ad-layout="in-article"
-  //     data-ad-format="fluid"
-  //     data-ad-client="ca-pub-9194552698690511"
-  //     data-ad-slot="7773865737"
-  //   />
-  //   <script dangerouslySetInnerHTML={{
-  //     __html: `
-  //     (adsbygoogle = window.adsbygoogle || []).push({});
-  //   `,
-  //   }}
-  //   />
-  // </div>
-  // );
+  return window.localStorage.getItem('cookiesAdsense')
+    && (
+    <div style={{ display: 'block', textAlign: 'center' }}>
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block', textAlignt: 'center' }}
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-9194552698690511"
+        data-ad-slot="7773865737"
+      />
+      <script dangerouslySetInnerHTML={{
+        __html: `
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      `,
+      }}
+      />
+    </div>
+    );
 }
